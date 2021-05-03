1246f2fd68d3899d847460d7e8107e46368a0bc7
Harish <harish46715@gmail.com>
2021-05-03 12:07:35 +0530

    second commit

--- counter/app.js
+++ counter/app.js
@@ -1,25 +1,25 @@
 let count = 0; //initial count
 
-const countVal = document.querySelector('.countval');
-const btns = document.querySelectorAll('.btn');
+const countVal = document.querySelector(".countval");
+const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
-  btn.addEventListener('click', function (e) {
+  btn.addEventListener("click", function (e) {
     const list = e.currentTarget.classList;
-    if (list.contains('decrease')) {
+    if (list.contains("decrease")) {
       count--;
-    } else if (list.contains('reset')) {
+    } else if (list.contains("reset")) {
       count = 0;
-    } else if (list.contains('increase')) {
+    } else if (list.contains("increase")) {
       count++;
     }
 
     if (count > 0) {
-      countVal.style.color = 'green';
+      countVal.style.color = "green";
     } else if (count < 0) {
-      countVal.style.color = 'red';
+      countVal.style.color = "red";
     } else if (count === 0) {
-      countVal.style.color = 'black';
+      countVal.style.color = "black";
     }
 
     countVal.textContent = count;
